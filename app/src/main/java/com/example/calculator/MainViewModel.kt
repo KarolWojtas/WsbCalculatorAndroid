9907ab27b20c3d733e72319da9bc578566c096c0
KarolWojtas <karol.wojtas.aim@gmail.com>
2020-10-17 14:49:46 +0200

    added calculate method

--- app/src/main/java/com/example/calculator/MainViewModel.kt
+++ app/src/main/java/com/example/calculator/MainViewModel.kt
@@ -8,7 +8,10 @@ import com.example.calculator.model.CalcNode
 import com.example.calculator.model.NumberNode
 import com.example.calculator.model.OperatorNode
 
+const val defaultEnteredValue = "0"
+
 class MainViewModel: ViewModel() {
+
     private val _nodes: MutableLiveData<MutableList<CalcNode<Any>>> = MutableLiveData(mutableListOf())
 
     val nodes: LiveData<MutableList<CalcNode<Any>>>
@@ -18,25 +21,66 @@ class MainViewModel: ViewModel() {
         _nodes.value = _nodes.value?.apply { add(node) }
     }
 
-    private val _enteredNumber: MutableLiveData<String> = MutableLiveData("0")
+    private val _enteredNumber = MutableLiveData(defaultEnteredValue)
 
     val enteredNumber: LiveData<String>
     get() = _enteredNumber
 
-    fun addNumber(value: Number){
-        if(_nodes.value?.size == 1)
+    private fun addNumber(value: Number){
+        // if(_nodes.value?.size == 1)
         addNode(NumberNode(value))
     }
 
+    /**
+     * adding operator first appends entered value to node list if defined
+     * secondly if operator was already added as last node, we replace it (only one operator between numbers)
+     */
     fun addOperator(value: String){
+        val enterNumberNumerical = _enteredNumber.value?.toIntOrNull()
+        if(enterNumberNumerical != null && enterNumberNumerical != 0){
+            addNumber(enterNumberNumerical)
+            _enteredNumber.value = defaultEnteredValue
+        }
+        val lastNode = _nodes.value?.lastOrNull()
+        if(lastNode is OperatorNode){
+            _nodes.value?.removeLast()
+        }
+
         addNode(OperatorNode(value))
     }
 
     fun appendDigit(value: Number){
-        _enteredNumber.value = if(_enteredNumber.value == "0") value.toString() else _enteredNumber.value+value
+        _enteredNumber.value = if(_enteredNumber.value == defaultEnteredValue) value.toString() else _enteredNumber.value+value
     }
 
     val nodesLiteral = nodes.map {
         it.joinToString(" ")
+    }
+
+    fun reset(){
+        _enteredNumber.value = defaultEnteredValue
+        _nodes.value = mutableListOf()
+    }
+
+    /**
+     * present
+     */
+    fun equals(){
+        addOperator("=")
+        _nodes.value?.let {
+            var currentOperator: OperatorNode? = null
+            var accumulator = 0.0
+            for ( nodeIx in it.indices){
+                val currentNode = it[nodeIx]
+                if(currentNode is OperatorNode){
+                    currentOperator = currentNode
+                } else if (currentNode is NumberNode){
+                    accumulator = currentOperator?.calculate(accumulator, currentNode.value.toDouble())?:currentNode.value.toDouble()
+                }
+            }
+            addNumber(if(accumulator % 1 != 0.0) accumulator else accumulator.toInt())
+            _enteredNumber.value = defaultEnteredValue
+        }
+
     }
 }
--- app/src/main/java/com/example/calculator/model/CalcModel.kt
+++ app/src/main/java/com/example/calculator/model/CalcModel.kt
@@ -1,12 +1,10 @@
 package com.example.calculator.model
 
-import android.view.View
-
-enum class CalcNodeTypes {
-    operator, digit
+enum class OperatorTypes(val value: String) {
+    plus("+"), minus("-"), multiply("*"), divide("/")
 }
 
-abstract class CalcNode<out T> (val type: CalcNodeTypes, val value: T){
+abstract class CalcNode<out T> (val value: T){
     override fun toString(): String {
         return value.toString()
     }
@@ -16,17 +14,28 @@ abstract class CalcNode<out T> (val type: CalcNodeTypes, val value: T){
     }
 
     override fun hashCode(): Int {
-        var result = type.hashCode()
-        result = 31 * result + (value?.hashCode() ?: 0)
-        return result
+        return value?.hashCode() ?: 0
     }
+
 }
 
-class NumberNode(value: Number) : CalcNode<Number>(CalcNodeTypes.digit, value){
+class NumberNode(value: Number) : CalcNode<Number>(value){
     companion object {
         val zero: NumberNode
         get() = NumberNode(0)
     }
 }
 
-class OperatorNode(value: String): CalcNode<String>(CalcNodeTypes.operator, value)
+class OperatorNode(value: String): CalcNode<String>(value) {
+    private val availableOperations: List<String> = OperatorTypes.values().map { it.value }
+    fun calculate(a: Double, b: Double): Double{
+        return when(value){
+            !in availableOperations -> a
+            OperatorTypes.plus.value -> a + b
+            OperatorTypes.minus.value -> a - b
+            OperatorTypes.multiply.value -> a * b
+            OperatorTypes.divide.value -> a / b
+            else -> a
+        }
+    }
+}
